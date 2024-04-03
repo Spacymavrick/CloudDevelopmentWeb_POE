2902574466f72cfd8e87e802f8ce022b725f4a2f
Spacymavrick <96030302+Spacymavrick@users.noreply.github.com>
2024-04-03 20:58:09 +0200

    Add project files.

---
+++ CloudDevelopmentWeb/Pages/Contact_Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudDevelopmentWeb.Pages
+{
+    public class Contact_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CloudDevelopmentWeb/Pages/MyWork_Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudDevelopmentWeb.Pages
+{
+    public class MyWork_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
