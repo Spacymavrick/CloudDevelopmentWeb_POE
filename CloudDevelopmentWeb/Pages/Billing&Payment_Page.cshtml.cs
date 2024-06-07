e58db9e629b057a7aa8f5242aeec8bfa5e318900
Spacymavrick <96030302+Spacymavrick@users.noreply.github.com>
2024-06-07 15:45:57 +0200

    Update

---
+++ CloudDevelopmentWeb/Pages/Billing&Payment_Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudDevelopmentWeb.Pages
+{
+    public class Billing_Payment_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
